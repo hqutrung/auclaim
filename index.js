f483d5682c7eb9c977bbf2ccf7da91b257e7461a
Trung <trunghuynh1999@gmail.com>
2023-01-03 16:10:26 +0700

    update code

---
+++ index.js
@@ -0,0 +1,10 @@
+function getAddress(blockChain) {
+  console.log("getAddress");
+  //   return window.flutter_inappwebview.callHandler("getAddress", {
+  //     block_chain: blockChain,
+  //   });
+}
+
+define(function (require, exports, module) {
+  module.exports = getAddress;
+});
